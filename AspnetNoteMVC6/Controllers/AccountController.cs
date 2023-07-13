bad3714a9115497d2ae815d6fce49529dd9e8b66
qkrthgus7026@kumoh.ac.kr <wldnjs7202!>
2023-07-13 15:10:09 +0900

    초기 커밋.

---
+++ AspnetNoteMVC6/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using AspnetNoteMVC6.DataContext;
+using AspnetNoteMVC6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetNoteMVC6.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if(ModelState.IsValid) //IsValid 사용자 이름 id pw 모두 받앗나 체크 null이면 false되어 유징 안돌아감
+            {
+                // Java try(SqlSession){} catch(){}
+                //C# using
+                using (var db = new AspnetNoteDbContext())
+                {
+                    db.Users.Add(model); // 메모리에 올림
+                    db.SaveChanges(); // 실제 sql에 저장
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ AspnetNoteMVC6/DataContext/AspnetNoteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspnetNoteMVC6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetNoteMVC6.DataContext
+{
+    public class AspnetNoteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=AspnetnoteDb;User Id=sa;Password=sa1234;");
+        }
+    }
+}
